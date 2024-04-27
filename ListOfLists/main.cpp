38a0d3286e72bc68fe22a348fb8e0325246ef3b2
Angela Mahlke <angel.mahlke@gmail.com>
2024-04-27 13:16:00 -0300

    listoflists repair

--- ListOfLists/main.cpp
+++ ListOfLists/main.cpp
@@ -1,20 +1,20 @@
 // Nomes: Angela Mahlke Ranoff & Yuri Alexander da Costa Machado
 
-#include <iostream>
 #include <cctype>
+#include <iostream>
+#include <ctime>
 
 #include "Lista.hpp"
 
- /* Na linguagem C++ ou nas linguagens estudadas fazer o seguintes PROGRAMA:
-  Gerar N listas de tamanho M com números aleatórios.
-  Mostrar seus conteúdos.
-  Mostrar os números que aparecem nas N listas.
-  Mostrar os números que aparecem somente em uma das listas.
-  List listaGenerica = new List();
+/* Na linguagem C++ ou nas linguagens estudadas fazer o seguintes PROGRAMA:
+ Gerar N listas de tamanho M com números aleatórios.
+ Mostrar seus conteúdos.
+ Mostrar os números que aparecem nas N listas.
+ Mostrar os números que aparecem somente em uma das listas.
+ List listaGenerica = new List();
 */
 
-int main() 
-{
+int main() {
   srand(time(NULL));
   /* n - var. do nr de listas, m - var. do tam. das listas */
   int n, m;
@@ -26,25 +26,42 @@ int main()
   std::cin >> m;
 
   /* É necessário criar uma matriz de dados */
-    /* Alocar a memória necessária para alocar as 'n' listas */
-  Lista** listaGenerica = (Lista**)malloc(n*sizeof(Lista));
-  
+  /* Alocar a memória necessária para alocar as 'n' listas */
+  Lista **listaGenerica = (Lista **)malloc(n * sizeof(Lista));
+
   /* Colocando os valores nas listas */
-  for (int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
       listaGenerica[i] = inserir(rand() % 200, listaGenerica[i]);
     }
   }
 
   /* Mostrar conteúdo das listas */
-  for (int i = 0; i < n; i++){
+  for (int i = 0; i < n; i++) {
     std::cout << "Lista " << i + 1 << ": ";
     exibir(listaGenerica[i]);
     std::cout << std::endl;
   }
 
-  free(listaGenerica);
+  Lista** ListaAuxiliar = listaGenerica;
   
+  std::cout << "\n\nNúmeros que aparecem nas n listas: ";
+  for (int i=0; i < n; i++) {
+    /* Verificação dos valores */
+    
+    for (Lista *p = ListaAuxiliar[i]; p != NULL; p = p->prox) {
+      if (contains(p->dado, listaGenerica[i]))
+        std::cout << p->dado << " ";
+    }
+  }
+
+  for(int i = 0; i < n; i++){
+    std::cout << "\n\nNúmeros que aparecem somente na lista " << i + 1 << std::endl;
+    for (Lista *p = listaGenerica[i]; p != NULL; p = p->prox) {
+      if (!contains(p->dado, listaGenerica[i + 1]))
+        std::cout << p->dado << " ";
+    }
+  }
+
+  free(listaGenerica);
 }
-
-
